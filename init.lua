814e63699d0b95bd8acc43d7085224dd06f95773
Callica <125240398+Callica@users.noreply.github.com>
2024-11-16 03:07:04 -0300

    Delete init.lua

--- init.lua
+++
@@ -1,7 +0,0 @@
--- Inicializador para o script
-local gui = "https://raw.githubusercontent.com/Callica/Terapia/main/gui.lua"
-local services = "https://raw.githubusercontent.com/Callica/Terapia/main/services.lua"
-
--- Carregar os módulos
-loadstring(game:HttpGet(gui))()
-loadstring(game:HttpGet(services))()
